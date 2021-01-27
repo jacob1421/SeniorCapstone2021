7a6e075305a0c087e60422fb0d67893e2337c37b
Jacob Bremiller <48461435+jacob1421@users.noreply.github.com>
2021-01-27 11:31:21 -0600

    Name fix
    ChocAon to ChocAnon

--- src/ChocAnon/ChocAnon.java
+++ src/ChocAnon/ChocAnon.java
@@ -1,5 +1,5 @@
 /*
-    File: ChocAon.java
+    File: ChocAnon.java
     Project: Capstone Project Team #1
     University: McMurry University
     Course: COSC-4360 Spring 2021
@@ -13,18 +13,18 @@
     Description: The main file of the project
 */
 
-package ChocAon;
+package ChocAnon;
 
 /**
  *
  * @author brozovir
  */
-public class ChocAon extends javax.swing.JFrame {
+public class ChocAnon extends javax.swing.JFrame {
 
     /**
      * Creates new form Program_Name
      */
-    public ChocAon() {
+    public ChocAnon() {
         initComponents();
     }
 
@@ -83,21 +83,23 @@ public class ChocAon extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ChocAon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChocAnon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ChocAon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChocAnon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ChocAon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChocAnon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ChocAon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChocAnon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ChocAon().setVisible(true);
+                new ChocAnon().setVisible(true);
             }
         });
     }
